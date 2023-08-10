382b90080437700821720345df05b10c167f6be2
Hamed <hamedpyn@gmail.com>
2023-08-10 15:23:32 +0430

    Select DOM Elements

---
+++ assets/src/js/index.js
@@ -0,0 +1,14 @@
+// Select DOM Elements
+
+// button to trigger text-to-speech
+let soundBtn = document.querySelector('#sound'),
+    // element containing quote text
+    quoteText = document.querySelector('#quote'),
+    // button to fetch new quote
+    quoteBtn = document.querySelector('#button'),
+    // element containing quote author
+    writer = document.querySelector('#quoteWriter'),
+    // button to tweet quote
+    twitter = document.querySelector('#twitter'),
+    // button to copy quote to clipboard
+    copy = document.querySelector('#copy');
